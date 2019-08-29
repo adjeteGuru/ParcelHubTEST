12e858560b82472b5413768dfac89eb91945f5e5
adjeteGuru <justinadjev@hotmail.com>
2019-08-29 16:02:23 +0100

    setup master page, Tested product list and successfully

---
+++ ParcelHubWebTest/ParcelHubWebTest/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ParcelHubWebTest.Models;
+
+namespace ParcelHubWebTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        //create db database instance of the class
+        private ParcelHubDBEntities db = new ParcelHubDBEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            //list all from Product table from the Database
+            ViewBag.products = db.tblProducts.ToList();
+            return View();
+        }
+    }
+}
